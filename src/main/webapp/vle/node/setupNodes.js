9993007354dcc24909d8eba21a259b4ddcb0c8f3
Dan Troon <dtroon@filamentgames.com>
2012-08-29 12:46:31 -0500

    everything to do with surge

--- src/main/webapp/vle/node/setupNodes.js
+++ src/main/webapp/vle/node/setupNodes.js
@@ -119,6 +119,10 @@ SetupNodes.setupFiles = [
 	{
 		nodeName:"NetlogoNode",
 		nodeSetupPath:"vle/node/netlogo/setup.js"
+	},
+	{
+		nodeName:"EpigameNode",
+		nodeSetupPath:"vle/node/epigame/setup.js"
 	}
 ];
 
